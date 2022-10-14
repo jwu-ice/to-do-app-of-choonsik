717bcfd7a82af54296df988928a1ce6cce6bf66e
jwu-ice <sju02048@naver.com>
2022-10-14 21:55:10 +0900

    Feat bg image 자동화 함수 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,33 @@
+let TODO_ITEMS_COUNT = 8
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{jsx,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        ...repeatObject("checkbox-", "checkBox", TODO_ITEMS_COUNT, "assets/", "png"),
+        "choonsik-bg": "url('assets/choonsik-bg.png')",
+        "choonsik-title": "url('assets/choonsik-title.png')",
+      },
+    },
   },
   plugins: [],
-};
+}
+
+function repeatObject(
+  utilityName,
+  imageName,
+  repeatNumber = 1,
+  path = "assets/",
+  extension = "png",
+) {
+  const object = [...Array(repeatNumber)].reduce((prev, _, i) => {
+    prev[`${utilityName}${i + 1}`] = `url(${path}${imageName}${i + 1}.${extension})`
+    return prev
+  }, {})
+
+  console.log("object", object)
+
+  return object
+}
