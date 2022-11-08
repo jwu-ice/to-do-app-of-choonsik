25a6ea3ed935997ce5086eeb372218beec1a188d
jwu-ice <sju02048@naver.com>
2022-11-08 17:01:27 +0900

    Update use repeatObject and Safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,24 @@
 const { TODO_ITEMS_COUNT } = require("./src/settings")
-const { repeatObject } = require("./src/utils")
+const { repeatObject } = require("./src/utils/repeatObject")
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  safelist: [{ pattern: /bg-(checkbox|line)-d*/ }],
   theme: {
     extend: {
       backgroundImage: {
         ...repeatObject({
-          utilityName: "checkbox-",
+          className: "checkbox-",
           imageName: "checkBox",
           repeat: TODO_ITEMS_COUNT,
         }),
+        ...repeatObject({
+          className: "line-",
+          imageName: "line",
+          repeat: TODO_ITEMS_COUNT,
+        }),
         "choonsik-bg": "url('assets/choonsik-bg.png')",
-      },
-      backgroundSize: {
-        16: "4rem",
       },
     },
   },
