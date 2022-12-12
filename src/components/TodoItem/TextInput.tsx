ff8918ea3e411a0ba37b9661c17a7839a7e9042e
jwu-ice <sju02048@naver.com>
2022-12-12 18:50:55 +0900

    Update use flushsync

--- src/components/TodoItem/TextInput.tsx
+++ src/components/TodoItem/TextInput.tsx
@@ -1,34 +1,69 @@
-import { ChangeEvent, useRef } from "react"
+import { ChangeEvent, useCallback, useEffect, useRef } from "react"
+import { useRecoilState } from "recoil"
+import { useState } from "react"
+import { flushSync } from "react-dom"
+import { atomFamilyTodo } from "@/store/atoms"
 
-const TextInput = (props: {
-  text: string
-  handleChangeText: (e: any) => void
-}) => {
-  const { text, handleChangeText } = props
-  const textareaRef = useRef<HTMLTextAreaElement>(null)
-
-  const handleChangeHeight = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    handleChangeText(e)
-    e.currentTarget.style.height = "auto"
-    e.currentTarget.style.height = textareaRef.current?.scrollHeight + "px"
+const TextInput = ({ id }: { id: number }) => {
+  const [height, setHeight] = useState<undefined | number | string>(32)
+  const [todo, setTodo] = useRecoilState(atomFamilyTodo(id))
+  const { text } = todo
+  const [a, setA] = useState(0)
+  const handles = () => {
+    setA((t) => t + 1)
   }
 
+  const textareaRef = useRef<HTMLTextAreaElement>(null)
+
+  const handleChangeValue = useCallback(
+    (e: ChangeEvent<HTMLTextAreaElement>) => {
+      const currentValue = e.target.value
+
+      setTimeout(() => {
+        currentValue === e.target.value &&
+          setTodo({ ...todo, text: currentValue })
+      }, 300)
+
+      flushSync(() => {
+        setHeight("auto")
+      })
+      flushSync(() => {
+        setHeight(textareaRef.current?.scrollHeight + "px")
+      })
+    },
+    [text],
+  )
+
+  useEffect(() => {
+    flushSync(() => {
+      setHeight("auto")
+    })
+    flushSync(() => {
+      setHeight(textareaRef.current?.scrollHeight + "px")
+    })
+  }, [])
+
   return (
-    <textarea
-      ref={textareaRef}
-      onChange={handleChangeHeight}
-      className={`text-2xl w-full px-3 overflow-hidden bg-transparent outline-none resize-none focus:ring-4 focus:ring-blue-400 focus:rounded-2xl placeholder:text-black placeholder:opacity-50`}
-      defaultValue={text}
-      placeholder="할 일 입력하기"
-      rows={1}
-      aria-label="할 일 입력창"
-      aria-valuetext={text}
-    />
+    <>
+      <p contentEditable>22</p>
+      <textarea
+        onKeyDown={(e) => {
+          if (e.key === "Enter") e.preventDefault()
+        }}
+        ref={textareaRef}
+        onChange={handleChangeValue}
+        style={{ height: height }}
+        className={`text-2xl w-full px-3 overflow-hidden bg-transparent outline-none resize-none focus:ring-4 focus:ring-blue-400 focus:rounded-2xl placeholder:text-black placeholder:opacity-50`}
+        defaultValue={text}
+        maxLength={255}
+        rows={1}
+        placeholder="할 일 입력하기"
+        aria-label="할 일 입력창"
+        aria-valuetext={text}
+        spellCheck="false"
+      />
+    </>
   )
 }
-
-// export default memo(TextInput, (prev, next) => {
-//   return prev.text === next.text
-// })
 
 export default TextInput
