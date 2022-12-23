b41965d6cd57ba80187c3dd30aa9182a49f31de6
jwu-ice <sju02048@naver.com>
2022-12-23 20:48:16 +0900

    Refactor mock json 삭제

--- src/settings/index.js
+++ src/settings/index.js
@@ -1,5 +1,10 @@
-// export const TODO_ITEMS_COUNT = 8
+module.exports = (() => {
+  let localStorageKey = "todo-"
 
-module.exports = {
-  TODO_ITEMS_COUNT: 8,
-}
+  return {
+    // TODO_ITEMS_COUNT: 8,
+    TODO_IDS_KEY_LOCALSTORAGE: `${localStorageKey}ids`,
+    TODO_ITEM_KEY_LOCALSTORAGE: localStorageKey,
+    TODO_LOADING_IMAGE: ["bg-choonsik-loading-1", "bg-choonsik-loading-2"],
+  }
+})()
