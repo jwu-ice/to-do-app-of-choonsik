4b6384cf14969f74f620d4c8a69c6ae2bb192122
Jwu <sju02048@naver.com>
2024-01-24 18:21:49 +0900

    Feat todo delete 폰에서도 적용

--- src/components/OptionList/index.tsx
+++ src/components/OptionList/index.tsx
@@ -1,4 +1,4 @@
-import { useRecoilState } from "recoil"
+import { useRecoilState, useRecoilValue } from "recoil"
 import { MouseEvent, memo, useCallback, useState } from "react"
 import XButton from "@/components/Common/XButton"
 import { atomLanguage } from "@/store/language"
@@ -14,12 +14,12 @@ import { TEXT_LOCAL, TypeLang } from "@/constants"
 const OptionList = () => {
   const [isOpen, setIsOpen] = useRecoilState<boolean>(atomOptionModal)
   const [isOpenColorPicker, setIsOpenColorPicker] = useState(false)
-  const [isOpenChangeLanguage, setIsOpenChangeLanguage] = useState(false)
-  const [language, setLanguage] = useRecoilState<TypeLang>(atomLanguage)
+  const language = useRecoilValue<TypeLang>(atomLanguage)
   const { dispatch } = useDispatchTodoIds()
 
   const handleClickAllDeleteTodoList = useCallback(() => {
     const isDeleteAll = confirm(TEXT_LOCAL[language].confirm_delete)
+
     if (isDeleteAll) {
       dispatch({ type: "DELETE_ALL" })
     }
@@ -44,43 +44,41 @@ const OptionList = () => {
             <div
               onClick={(e) => e.stopPropagation()}
               className={
-                "absolute top-1/2 left-1/2 z-20 h-auto w-80 -translate-x-1/2 -translate-y-1/2 rounded-lg bg-white p-5 shadow-xl"
+                "absolute top-1/3 left-1/2 z-20 h-auto w-80 -translate-x-1/2 -translate-y-1/3 rounded-lg bg-white p-5 shadow-xl"
               }
             >
               <div className="h-full space-y-2">
                 <h2 className="mb-3 text-xl ">
                   {TEXT_LOCAL[language].option_preference}
                 </h2>
-                {
-                  <>
-                    <OptionItem
-                      onClick={handleClickAllDeleteTodoList}
-                      lang={language}
-                      text={"option_do_delete_all"}
-                      tabIndex={1}
-                      className={"h-4 w-4"}
-                    >
-                      <CheckButton />
-                    </OptionItem>
-                    <OptionItem
-                      onClick={() => ""}
-                      lang={language}
-                      text={"option_change_background_color"}
-                      tabIndex={2}
-                    >
-                      <ColorPicker />
-                    </OptionItem>
+                <>
+                  <OptionItem
+                    onClick={handleClickAllDeleteTodoList}
+                    lang={language}
+                    text={"option_do_delete_all"}
+                    tabIndex={1}
+                    className={"h-4 w-4"}
+                  >
+                    <CheckButton />
+                  </OptionItem>
+                  <OptionItem
+                    onClick={() => ""}
+                    lang={language}
+                    text={"option_change_background_color"}
+                    tabIndex={2}
+                  >
+                    <ColorPicker />
+                  </OptionItem>
 
-                    <OptionItem
-                      onClick={() => ""}
-                      lang={language}
-                      text={"option_change_language"}
-                      tabIndex={3}
-                    >
-                      <SelectOption />
-                    </OptionItem>
-                  </>
-                }
+                  <OptionItem
+                    onClick={() => ""}
+                    lang={language}
+                    text={"option_change_language"}
+                    tabIndex={3}
+                  >
+                    <SelectOption />
+                  </OptionItem>
+                </>
                 <XButton
                   onClick={handleCloseOption}
                   className="absolute right-3 top-1 h-4 w-4"
--- src/components/TodoItem/Cancel.tsx
+++ src/components/TodoItem/Cancel.tsx
@@ -10,7 +10,7 @@ const Cancel = ({ id }: { id: number }) => {
     if (intervalRef.current) return
     intervalRef.current = window.setInterval(() => {
       dispatch({ type: "DELETE", targetId: id })
-    }, 500)
+    }, 300)
   }, [dispatch, id])
 
   const handleStopDelete = useCallback(() => {
@@ -31,6 +31,9 @@ const Cancel = ({ id }: { id: number }) => {
       onMouseDown={handleStartDelete}
       onMouseUp={handleStopDelete}
       onMouseLeave={handleStopDelete}
+      onTouchStart={handleStartDelete}
+      onTouchEnd={handleStopDelete}
+      onTouchCancel={handleStopDelete}
       className="group/cancel invisible absolute right-2 top-1/2 block h-5 w-5 -translate-y-1/2 rounded-full group-hover/item:visible active:shadow-animate-slide active:duration-500"
     >
       <XButton />
